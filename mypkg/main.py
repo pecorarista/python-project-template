2b79b1d3efe927740c17a930d7e5250b8898a647
Miyazawa Akira <pecorarista@gmail.com>
2019-10-21 00:45:31 +0900

    Fix main

--- mypkg/main.py
+++ mypkg/main.py
@@ -1,10 +1,11 @@
 import argparse
-import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 
+from mypkg.database.model import Base
 from mypkg.preprocessing.corpus import import_documents
+from mypkg.util.config import Config
 
 
 def main() -> None:
@@ -16,12 +17,20 @@ def main() -> None:
                         required=True,
                         metavar='DIR',
                         help='Directory which contains corpus files')
+    parser.add_argument('-c',
+                        '--config',
+                        type=str,
+                        required=False,
+                        default='config.toml',
+                        metavar='FILE',
+                        help='Path to config file (default: `config.toml`)')
 
     args = parser.parse_args()
+    config = Config(args.config)
 
     engine = create_engine(config.db_uri)
     Base.metadata.create_all(engine)
     SessionMaker = sessionmaker(bind=engine)
     db_session = SessionMaker()
 
-    import_documents(session, dir_corpus)
+    import_documents(db_session, args.dir_corpus)
