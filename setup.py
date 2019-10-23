eb224ed653a708c151d1d69b35f41751a52fbe05
Miyazawa Akira <pecorarista@gmail.com>
2019-10-23 12:18:02 +0900

    Update setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-
-setup(name='mypkg',
-      version='1.0',
-      description='Example Package',
-      author='Miyazawa Akira',
-      author_email='pecorarista@gmail.com',
-      url='https://github.com/pecorarista/python-project-template',
-      entry_points={"console_scripts": ["mypkg = mypkg.main:main"]},
-      test_suite='tests')
+setup()
